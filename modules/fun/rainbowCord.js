ad67238894560cf39f9e4143ce8efeb846cb77e6
Oj18 <oj@oojmed.com>
2020-09-26 08:44:54 +0100

    RainbowCord v2.0.0

--- modules/fun/rainbowCord.js
+++ modules/fun/rainbowCord.js
@@ -1,70 +1,74 @@
-let version = '1.0.0';
+let version = '2.0.0';
+
+if (typeof window === 'undefined' || typeof window.document === 'undefined' || typeof window.document.styleSheets === 'undefined') { // JSON API generator evals
+  global.window = { document: { styleSheets: [0] } };
+}
+
+let sheet = window.document.styleSheets[0];
+let tweakrules = [];
+
+let tweaks = {
+  'rainbowMode': true
+};
+
+const tweakFunctions = {
+  'rainbowMode': {
+    enable: () => {
+      tweakrules.push(sheet.insertRule(`.messageContent-2qWWxC {
+        background: repeating-linear-gradient(45deg, violet, indigo, blue, green, yellow, orange, red);
+        -webkit-background-clip: text;
+        color: transparent;
+        background-size: 800% 800%;
+        animation: rainbow 8s ease infinite;
+      }`, sheet.cssRules.length));
+
+      tweakrules.push(sheet.insertRule(`code.inline {
+        color: #fff;
+      }`, sheet.cssRules.length));
+
+      tweakrules.push(sheet.insertRule(`@keyframes rainbow { 
+        0%{background-position:0% 50%}
+        50%{background-position:100% 25%}
+        100%{background-position:0% 50%}
+      }`, sheet.cssRules.length));
+    },
+
+    disable: () => {
+      for (let i = 0; i < tweakrules.length; i++) {
+        sheet.deleteRule(tweakrules[i]);
+      }
+    }
+  }
+};
+
+const enableTweak = (tweakName) => {
+  tweakFunctions[tweakName].enable();
+
+  tweaks[tweakName] = true;
+};
+
+const disableTweak = (tweakName) => {
+  tweakFunctions[tweakName].disable();
+
+  tweaks[tweakName] = false;
+};
+
+const setTweak = (tweakName, value) => {
+  if (value) {
+    enableTweak(tweakName);
+  } else {
+    disableTweak(tweakName);
+  }
+};
 
 let obj = {
   onImport: async function () {
     this.logger.debug('rainbowMode', 'Enabling Rainbow Tweaks');
-
-    this.r_tweaks = {
-      'rainbowMode': true
-    };
-
-    let sheet = window.document.styleSheets[0];
-    let tweakrules = [];
-
-    let tweakFunctions = {
-      'rainbowMode': {
-        enable: () => {
-          tweakrules.push(sheet.insertRule(`.messageContent-2qWWxC {
-            background: repeating-linear-gradient(45deg, violet, indigo, blue, green, yellow, orange, red);
-            -webkit-background-clip: text;
-            color: transparent;
-            background-size: 800% 800%;
-            animation: rainbow 8s ease infinite;
-          }`, sheet.cssRules.length));
-
-          tweakrules.push(sheet.insertRule(`code.inline {
-            color: #fff;
-          }`, sheet.cssRules.length));
-
-          tweakrules.push(sheet.insertRule(`@keyframes rainbow { 
-            0%{background-position:0% 50%}
-            50%{background-position:100% 25%}
-            100%{background-position:0% 50%}
-          }`, sheet.cssRules.length));
-        },
-
-        disable: () => {
-          for (let i = 0; i < tweakrules.length; i++) {
-            sheet.deleteRule(tweakrules[i]);
-          }
-        }
-      }
-    };
-
-    this.r_enableTweak = (tweakName) => {
-      tweakFunctions[tweakName].enable();
-
-      this.r_tweaks[tweakName] = true;
-    };
-
-    this.r_disableTweak = (tweakName) => {
-      tweakFunctions[tweakName].disable();
-
-      this.r_tweaks[tweakName] = false;
-    };
-
-    this.r_setTweak = (tweakName, value) => {
-      if (value === true) {
-        this.r_enableTweak(tweakName);
-      } else {
-        this.r_disableTweak(tweakName);
-      }
-    };
   },
 
   onLoadingFinished: async function () {
-    for (let t in this.r_tweaks) {
-      if (this.r_tweaks[t] === true) this.r_enableTweak(t);
+    for (let t in tweaks) {
+      if (tweaks[t] === true) enableTweak(t);
     }
 
     this.settings.createItem('RainbowCord', [
@@ -78,15 +82,15 @@ let obj = {
         type: 'toggle',
         text: 'Rainbow Mode',
         subtext: 'Messages are rainbow',
-        onToggle: (c) => { this.r_setTweak('rainbowMode', c); },
-        isToggled: () => this.r_tweaks['rainbowMode']
+        onToggle: (c) => { setTweak('rainbowMode', c); },
+        isToggled: () => tweaks['rainbowMode']
       }
     ]);
   },
 
   remove: async function () {
-    for (let t in this.r_tweaks) {
-      if (this.r_tweaks[t] === true) this.r_disableTweak(t);
+    for (let t in this.tweaks) {
+      if (this.tweaks[t] === true) disableTweak(t);
     }
 
     let settingItem = this.settings.items.find((x) => x[1] === 'RainbowCord');
