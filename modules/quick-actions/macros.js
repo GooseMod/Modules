309d8fe970ae57945e94b327b81df077ab1d87c9
Oj18 <oj@oojmed.com>
2020-10-02 22:11:44 +0100

    Macros v1.0.0

---
+++ modules/quick-actions/macros.js
@@ -0,0 +1,66 @@
+let originalFunc;
+
+let macros = {};
+
+let version = '1.0.0';
+
+let obj = {
+  onImport: async function () {
+    let mod = this.webpackModules.findByProps("jumpToMessage", "_sendMessage");
+    originalFunc = mod.sendMessage;
+
+    let goosemodScope = this;
+
+    mod.sendMessage = function (_, message) {//_, [, message]) {
+      if (message.content.startsWith('/m')) {
+        let rpl = message.content.replace('/m ', '');
+        let name = rpl.split(' ')[0];
+        let content = rpl.split(' ').slice(1).join(' ');
+
+        console.log(macros, name, content);
+
+        if (!content) {
+          if (!macros[name]) {
+            goosemodScope.showToast(`No macro "${name}"`);
+
+            return;
+          }
+
+          message.content = macros[name];
+        } else {
+          macros[name] = content;
+
+          goosemodScope.showToast(`Macro "${name}" saved`);
+
+          return;
+        }
+      }
+      
+      return originalFunc.apply(this, arguments);
+    };
+  },
+
+  onLoadingFinished: async function() {
+    
+  },
+
+  remove: async function () {
+    this.webpackModules.findByProps("jumpToMessage", "_sendMessage").sendMessage = originalFunc;
+  },
+
+  getSettings: () => [macros],
+  loadSettings: ([_macros]) => {
+    macros = _macros;
+  },
+
+  logRegionColor: 'darkred',
+
+  name: 'Macros',
+  description: 'Allows setting and sending of macros via /m name [value]',
+
+  author: 'Ducko',
+
+  version
+};
+
+obj
