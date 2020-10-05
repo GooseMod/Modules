61bd3d9590a2b5f93d5baa66e5781a656da829f0
Oj18 <oj@oojmed.com>
2020-10-05 20:38:02 +0100

    Username In Author v1.0.0: Initial Release

---
+++ modules/messages/usernameInAuthor.js
@@ -0,0 +1,73 @@
+const version = '1.0.0';
+
+let getUser, getMembers, getGuildId;
+let members;
+let intervals;
+
+const run = () => {
+  for (let el of document.getElementsByClassName('username-1A8OIy')) {
+    if (el.classList.contains('username-added')) continue;
+
+    let nick = el.textContent;
+    let member = members.find((x) => x.nick === nick);
+
+    if (!member) continue;
+
+    let user = getUser.getUser(member.userId);
+
+    let usernameEl = document.createElement('span');
+    usernameEl.style.filter = 'brightness(0.5)';
+    usernameEl.style.marginLeft = '5px';
+    usernameEl.className = 'author-username';
+
+    usernameEl.textContent = `(${user.username})`;
+
+    el.appendChild(usernameEl);
+
+    el.classList.add('username-added');
+  }
+};
+
+const updateMembers = () => {
+  members = getMembers.getMembers(getGuildId.getGuildId());
+};
+
+let obj = {
+  onImport: async function () {
+    intervals = [];
+
+    getUser = this.webpackModules.findByProps('getUser');
+    getMembers = this.webpackModules.findByProps('getNick');
+    getGuildId = this.webpackModules.findByPropsAll('getGuildId')[1];
+
+    intervals.push(setInterval(run, 300), setInterval(updateMembers, 5000));
+
+    updateMembers();
+  },
+
+  onLoadingFinished: async function () {
+  },
+
+  remove: async function () {
+    for (let i of intervals) {
+      clearInterval(i);
+    }
+
+    members = null;
+
+    for (let el of document.getElementsByClassName('author-username')) {
+      el.remove();
+    }
+  },
+
+  logRegionColor: 'darkred',
+
+  name: 'Username In Author',
+  description: 'Adds username to message\'s author if they have a nickname',
+
+  author: 'Ducko',
+
+  version
+};
+
+obj
