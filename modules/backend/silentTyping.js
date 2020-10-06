0a18f0d81b9fa27677a08b86a616d482bd251951
Oj18 <oj@oojmed.com>
2020-10-06 20:18:47 +0100

    Silent Typing v1.0.0: Initial Release

---
+++ modules/backend/silentTyping.js
@@ -0,0 +1,31 @@
+const version = '1.0.0';
+
+let version = '1.0.0';
+let orig;
+
+let obj = {
+  onImport: async function () {
+    let mod = this.webpackModules.findByProps('startTyping');
+    orig = mod.startTyping;
+
+    mod.startTyping = () => {};
+  },
+
+  onLoadingFinished: async function () {
+    this.webpackModules.findByProps('startTyping').startTyping = orig;
+  },
+
+  remove: async function () {
+  },
+
+  logRegionColor: 'darkred',
+
+  name: 'Silent Typing',
+  description: 'Makes Discord not send your typing indicator',
+
+  author: 'Ducko',
+
+  version
+};
+
+obj
