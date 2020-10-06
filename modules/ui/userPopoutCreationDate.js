c5f1f719ccf88e84a9cbb1ad7a1f87cb50d08411
Oj18 <oj@oojmed.com>
2020-10-06 21:52:00 +0100

    User Popout Creation Date v1.0.0: Initial Release

---
+++ modules/ui/userPopoutCreationDate.js
@@ -0,0 +1,67 @@
+const version = '1.0.0';
+
+let getUser, getMembers, getGuildId;
+let interval;
+
+const run = () => {
+  let popout = document.getElementsByClassName('userPopout-3XzG_A')[0];
+  
+  if (!popout) return;
+  if (popout.classList.contains('more-info-added')) return;
+
+  let user = getUser.getUser(getMembers.getMembers(getGuildId.getGuildId()).find((x) => getUser.getUser(x.userId).username === popout.ariaLabel).userId);
+
+  let toCloneEl = popout.getElementsByClassName('headerTagWithNickname-3IthZD')[0];
+
+  if (!toCloneEl) {
+    let headerEl = popout.getElementsByClassName('headerTagNoNickname-ENx6no')[0];
+    
+    let parentEl = document.createElement('div');
+
+    headerEl.parentElement.appendChild(parentEl);
+
+    parentEl.outerHTML = `<div class="flex-1xMQg5 flex-1O1GKY horizontal-1ae9ci horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG justifyStart-2NDFzi alignCenter-1dQNNs noWrap-3jynv6" style="flex: 1 1 auto;"><div class="headerTagWithNickname-3IthZD headerTag-2pZJzA size14-3iUx6q nameTag-m8r81H"></div></div>`;
+
+    toCloneEl = popout.getElementsByClassName('headerTagWithNickname-3IthZD')[0];
+
+    headerEl.parentElement.style.flexDirection = 'column';
+  }
+
+  toCloneEl.parentElement.style.flexDirection = 'column';
+
+  let el = toCloneEl.cloneNode(false);
+
+  el.textContent = `Created On: ${user.createdAt.toDateString().split(' ').slice(1).join(' ')}`;
+
+  toCloneEl.parentElement.appendChild(el);
+
+  popout.classList.add('more-info-added');
+};
+
+let obj = {
+  onImport: async function () {
+    getUser = this.webpackModules.findByProps('getUser');
+    getMembers = this.webpackModules.findByProps('getNick');
+    getGuildId = this.webpackModules.findByPropsAll('getGuildId')[1];
+
+    interval = setInterval(run, 300);
+  },
+
+  onLoadingFinished: async function () {
+  },
+
+  remove: async function () {
+    clearInterval(interval);
+  },
+
+  logRegionColor: 'darkred',
+
+  name: 'User Popout Creation Date',
+  description: 'Adds user\'s creation date to popouts',
+
+  author: 'Ducko',
+
+  version
+};
+
+obj
