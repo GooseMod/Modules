4629722377fcd48acc170d0b6f8ee53ef8ad8c19
Oj18 <oj@oojmed.com>
2020-10-16 21:28:27 +0100

    Bump Custom Sounds to v1.3.10

--- modules/customisation/customSounds.js
+++ modules/customisation/customSounds.js
@@ -1,4 +1,4 @@
-const version = '1.3.9';
+const version = '1.3.10';
 
 if (typeof window === 'undefined' || typeof window.Audio === 'undefined') { // JSON API generator evals
   global.window = {Audio: {}};
