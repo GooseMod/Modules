c8b38b92230b7f0fef6aacd5068ba7ad349e7dc6
Oj18 <oj@oojmed.com>
2020-10-18 17:31:54 +0100

    Dev Mode v3.0.0: Rewrite again

--- modules/backend/devMode.js
+++ modules/backend/devMode.js
@@ -1,10 +1,10 @@
-let version = '2.0.0';
+let version = '3.0.0';
 
 let obj = {
   onImport: async function() {
     globalThis.logger.debug('devMode', 'Enabling Developer Mode');
 
-		globalThis.webpackModules.findByProps('isDeveloper').isDeveloper = 1;
+		Object.defineProperty(globalThis.webpackModules.findByProps('isDeveloper'), 'isDeveloper', { configurable: true, writable: true, value: 1 });
 	},
 	
 	remove: async function() {
