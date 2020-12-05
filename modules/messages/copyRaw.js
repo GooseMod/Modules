18070dc8f1ccdc13a2beb8f79433c6e99ac538df
ControlFreak707 <controlfreak707@gmail.com>
2020-12-05 02:04:00 -0800

    GH #20: New Copy Raw module

---
+++ modules/messages/copyRaw.js
@@ -0,0 +1,33 @@
+const version = "1.0.0";
+
+let obj = {
+  onImport: async () => {
+    goosemodScope.patcher.contextMenu.add("message", {
+      label: "Copy Raw",
+      action: (_originalArgs, extraInfo) => {
+        try {
+          if (extraInfo.message.content == "") {
+            return goosemod.showToast("Unable to copy", { type: "error" });
+          }
+
+          DiscordNative.clipboard.copy(extraInfo.message.content);
+          goosemod.showToast("Copied!", { type: "success", icon: false });
+        } catch (error) {
+          goosemod.showToast("An error has occured!", { type: "error" });
+          console.error(error);
+        }
+      },
+    });
+  },
+
+  remove: async () => {
+    goosemodScope.patcher.contextMenu.remove("Copy Raw");
+  },
+
+  name: "Copy Raw",
+  description: "Allows you to copy messages as they were when being written.",
+  author: "Liam The Protogen",
+  version,
+};
+
+obj
