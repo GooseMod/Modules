6434a9104c1ecced5b5de30804f9cb3fe98df2f7
ControlFreak707 <controlfreak707@gmail.com>
2020-12-06 15:45:17 -0800

    Add No Notes module to UI category

---
+++ modules/ui/noNotes.js
@@ -0,0 +1,33 @@
+const version = "1.0.0";
+
+let style;
+
+let obj = {
+  onImport: async () => {
+    style = document.createElement("style");
+    document.head.appendChild(style);
+    style.appendChild(
+      document.createTextNode(`.userInfoSectionHeader-CBvMDh, .note-QfFU8y {
+         display: none;
+      }
+      .infoScroller-3EYYns > .userInfoSection-2acyCx:first-of-type {
+         margin-top: -45px;
+      }
+      .topSectionNormal-2-vo2m {
+         z-index: 10;
+      }`)
+    );
+  },
+
+  remove: async () => {
+    style.remove();
+  },
+
+  name: "No Notes",
+  description:
+    "Hides the notes on user profile modals. (Does not apply to the user popout.)",
+  author: "Liam The Protogen",
+  version,
+};
+
+obj
