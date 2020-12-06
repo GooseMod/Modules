b96952fdbeaa6a95005a1b5f05901d892c58dec2
Oj18 <oj@oojmed.com>
2020-12-06 15:14:58 +0000

    Add duvbox theme (port)

---
+++ modules/themes/duvboxTheme.js
@@ -0,0 +1,28 @@
+const version = '0';
+
+let el;
+
+const css = `@import url("https://raw.githack.com/CircuitRCAY/Duvbox/master/duvbox.css");`;
+
+let obj = {
+  onImport: async function () {
+    el = document.createElement('style');
+
+    document.head.appendChild(el);
+
+    el.appendChild(document.createTextNode(css));
+  },
+
+  remove: async function () {
+    el.remove();
+  },
+
+  name: 'Duvbox Theme',
+  description: 'This is a theme, based on the Vim theme, named Gruvbox.',
+
+  author: 'CircuitRCAY',
+
+  version
+}
+
+obj
