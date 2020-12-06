e0052a9e3ddce513cf88a352d99327cefb198886
ControlFreak707 <controlfreak707@gmail.com>
2020-12-05 16:45:50 -0800

    Upgrades, people, upgrades!
    Made slight adjustments to the De-Nitro module. It's not much, but it's something.

--- modules/ui/deNitro.js
+++ modules/ui/deNitro.js
@@ -1,15 +1,13 @@
-const version = "1.0.0";
+const version = "1.0.1";
 
-let style;
-const css = `.buttons-3JBrkn > :nth-child(1), .channel-2QD9_O[href="/store"], .item-PXvHYJ[aria-controls="Discord Nitro-tab"], .wrapper-3nSjSv, .giftAction-yhDLaW, .upsellFooter-3coAfO, .buttons-3JBrkn > :nth-child(3), .navItem-3Wp_oJ[aria-controls="sticker-picker-tab-panel"] {
+const style = document.createElement("style");
+style.appendChild(document.createTextNode(`.buttons-3JBrkn > :nth-child(1), .channel-2QD9_O[href="/store"], .item-PXvHYJ[aria-controls="Discord Nitro-tab"], .wrapper-3nSjSv, .giftAction-yhDLaW, .upsellFooter-3coAfO, .buttons-3JBrkn > :nth-child(3), .navItem-3Wp_oJ[aria-controls="sticker-picker-tab-panel"] {
   display: none;
-}`;
+}`));
 
 let obj = {
   onImport: async () => {
-    style = document.createElement("style");
     document.head.appendChild(style);
-    style.appendChild(document.createTextNode(css));
   },
 
   remove: async () => {
