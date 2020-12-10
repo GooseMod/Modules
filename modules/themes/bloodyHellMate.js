4d13077d36c95709e27b8d342565c6f39d920f41
ControlFreak707 <controlfreak707@gmail.com>
2020-12-09 21:50:02 -0800

    [Module Add] Bloody Hell Mate

---
+++ modules/themes/bloodyHellMate.js
@@ -0,0 +1,27 @@
+const version = "0";
+
+let style;
+
+let obj = {
+  onImport: async () => {
+    goosemodScope.showToast("onImport()");
+    style = document.createElement("style");
+    document.head.appendChild(style);
+    style.appendChild(
+      document.createTextNode(
+        `@import url("https://raw.githack.com/MrDragonBoi/bloody-hell-mate/main/index.css")`
+      )
+    );
+  },
+
+  remove: async () => {
+    style.remove();
+  },
+
+  name: "Bloody Hell Mate",
+  description: "Makes everything bloody red, mate!",
+  author: ["MrDragonBoi", "Liam The Protogen"],
+  version,
+};
+
+obj
