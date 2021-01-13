9532b7908ad1abd7b8dfa7df8ef6bcc2f84e84bb
alethéia <34745171+xirreal@users.noreply.github.com>
2021-01-13 14:50:45 +0100

    upload no ping reply

---
+++ modules/backend/noPingReply.js
@@ -0,0 +1,22 @@
+
+let version = '1.0';
+let unpatch;
+
+let obj = {
+  onImport: async function() {
+    unpatch = goosemodScope.patcher.patch(goosemodScope.webpackModules.findByProps('createPendingReply'), 'createPendingReply', (data) => { data[0].shouldMention = false}, true);
+  },
+
+  remove: async function() {
+    unpatch()
+  },
+
+  name: `Disable ping reply`,
+  description: `Automatically turns off pinging whoever you're replying to`,
+
+  author: 'Lith',
+
+  version
+};
+
+obj
