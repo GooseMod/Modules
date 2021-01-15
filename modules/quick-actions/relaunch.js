07d3eaa764c0257aab928696f67941573992bb00
ControlFreak707 <controlfreak707@gmail.com>
2021-01-15 13:50:15 -0800

    GH #66: [Module Update - Relaunch] Prevent modal from being summoned more than once

--- modules/quick-actions/relaunch.js
+++ modules/quick-actions/relaunch.js
@@ -1,27 +1,35 @@
-let version = "1.2.0";
+const version = "1.2.1";
 
-const openModal = async () => {
-  if (
-    !(await goosemodScope.confirmDialog(
-      "Relaunch",
-      "Relaunch Discord",
-      "Are you sure you want to relaunch Discord? This will completely terminate Discord and launch the updater."
-    ))
-  ) {
-    return;
-  }
-  DiscordNative.app.relaunch();
-};
-
-const keydownHandler = (event) => {
-  if (event.code == "F4") {
-    openModal();
-    event.preventDefault();
-  }
-};
+let keydownHandler;
 
 let obj = {
   onImport: async () => {
+    const openModal = async () => {
+      if (
+        document.querySelector(
+          ".focusLock-Ns3yie[aria-label='Relaunch Discord']"
+        )
+      )
+        return;
+
+      if (
+        await goosemodScope.confirmDialog(
+          "Relaunch",
+          "Relaunch Discord",
+          "Are you sure you want to relaunch Discord? This will completely terminate Discord and launch the updater."
+        )
+      ) {
+        DiscordNative.app.relaunch();
+      }
+    };
+
+    keydownHandler = async (event) => {
+      if (event.code == "F4") {
+        openModal();
+        event.preventDefault();
+      }
+    };
+
     goosemodScope.settings.createItem(
       "Relaunch",
       [""],
@@ -41,9 +49,7 @@ let obj = {
 
   name: "Relaunch",
   description: "Easily relaunch Discord, completely. (Press `F4` to relaunch.)",
-
   author: "Liam The Protogen (441384103946878987)",
-
   version,
 };
 
