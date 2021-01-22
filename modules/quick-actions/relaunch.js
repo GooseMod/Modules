6e21a4b00d67944e50142feba8646db68f4ce9a5
ControlFreak707 <controlfreak707@gmail.com>
2021-01-22 07:03:28 -0800

    GH #69: [Module Update - Relaunch] Use React to create ConfirmModal element

--- modules/quick-actions/relaunch.js
+++ modules/quick-actions/relaunch.js
@@ -1,32 +1,42 @@
-const version = "1.2.1";
+const version = "1.3.0";
 
 let keydownHandler;
 
 let obj = {
   onImport: async () => {
-    const openModal = async () => {
-      if (
-        document.querySelector(
-          ".focusLock-Ns3yie[aria-label='Relaunch Discord']"
-        )
-      )
-        return;
+    const createModal = async () => {
+      const { React } = goosemodScope.webpackModules.common;
+      const { findByDisplayName, findByProps } = goosemodScope.webpackModules;
 
-      if (
-        await goosemodScope.confirmDialog(
-          "Relaunch",
-          "Relaunch Discord",
-          "Are you sure you want to relaunch Discord? This will completely terminate Discord and launch the updater."
-        )
-      ) {
-        DiscordNative.app.relaunch();
-      }
+      const Text = findByDisplayName("Text");
+
+      (0, findByProps("openModal").openModal)((e) => {
+        return React.createElement(
+          findByDisplayName("ConfirmModal"),
+          {
+            header: "Relaunch",
+            confirmText: "Relaunch",
+            cancelText: findByProps("Messages").Messages.CANCEL,
+            onClose: () => {},
+            onCancel: e.onClose,
+            onConfirm: () => {
+              return DiscordNative.app.relaunch();
+            },
+            transitionState: e.transitionState,
+          },
+          React.createElement(
+            Text,
+            { size: Text.Sizes.SIZE_16 },
+            "Are you sure you want to relaunch Discord? This will completely terminate Discord and start the updater."
+          )
+        );
+      });
     };
 
     keydownHandler = async (event) => {
       if (event.code == "F4") {
-        openModal();
         event.preventDefault();
+        createModal();
       }
     };
 
@@ -34,7 +44,7 @@ let obj = {
       "Relaunch",
       [""],
       async () => {
-        openModal();
+        createModal();
       },
       true
     );
@@ -49,7 +59,7 @@ let obj = {
 
   name: "Relaunch",
   description: "Easily relaunch Discord, completely. (Press `F4` to relaunch.)",
-  author: "Liam The Protogen (441384103946878987)",
+  author: "441384103946878987",
   version,
 };
 
